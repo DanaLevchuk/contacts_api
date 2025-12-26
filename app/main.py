ec13812263faa31b551593c16a6dc81fb644966b
mac <danalevchuk251@gmail.com>
2025-12-26 15:43:04 +0100

    Auth, JWT, protected contacts endpoints

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from app.database import engine, Base
+from app import models
 from app.routes import router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-app.include_router(router)
+
+app.include_router(router, prefix="/auth", tags=["auth"])
+app.include_router(router, prefix="/contacts", tags=["contacts"])
--- app/models.py
+++ app/models.py
@@ -1,16 +1,14 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from app.database import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-
-    contacts = relationship("Contact", back_populates="owner")
-
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_verified = Column(Boolean, default=False)
 
 class Contact(Base):
     __tablename__ = "contacts"
